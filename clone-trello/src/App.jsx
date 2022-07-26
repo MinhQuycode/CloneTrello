a6b84232459bfe1ea99c18a82ae5ef2e9eba2e26
Quy Pham <quy.pham@hdwebsoft.co>
2022-07-27 00:16:58 +0700

    update layout trello
    :wm

--- clone-trello/src/App.jsx
+++ clone-trello/src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">Project Clone Trello</div>;
+  return (
+    <div className="app-trello-quypham">
+      <nav className="navbar app">Navbar App</nav>
+      <nav className="navbar board">Navbar Board</nav>
+      <div className="board-column">Board column</div>
+    </div>
+  );
 }
 
 export default App;
