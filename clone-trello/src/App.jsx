3019c4aed67f2afc1c98b096d2077ab044876f7e
Quy Pham <quy.pham@hdwebsoft.co>
2022-09-07 17:49:42 +0700

    Build layout trello

--- clone-trello/src/App.jsx
+++ clone-trello/src/App.jsx
@@ -5,7 +5,97 @@ function App() {
     <div className="app-trello-quypham">
       <nav className="navbar app">Navbar App</nav>
       <nav className="navbar board">Navbar Board</nav>
-      <div className="board-column">Board column</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="Img my work" />
+              Title: MinhQuyCode
+            </li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="Img my work" />
+              Title: MinhQuyCode
+            </li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="Img my work" />
+              Title: MinhQuyCode
+            </li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="Img my work" />
+              Title: MinhQuyCode
+            </li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="Img my work" />
+              Title: MinhQuyCode
+            </li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="Img my work" />
+              Title: MinhQuyCode
+            </li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+            <li>Add what you like to work ?</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
