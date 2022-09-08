ef56c0838951db0902a211671b8c7e3099c4c60d
Quy Pham <quy.pham@hdwebsoft.co>
2022-09-08 16:27:27 +0700

    clean react architecture

--- clone-trello/src/App.jsx
+++ clone-trello/src/App.jsx
@@ -1,101 +1,15 @@
 import "./App.scss";
+// Custom component
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 
 function App() {
   return (
     <div className="app-trello-quypham">
-      <nav className="navbar app">Navbar App</nav>
-      <nav className="navbar board">Navbar Board</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/200/300" alt="Img my work" />
-              Title: MinhQuyCode
-            </li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/200/300" alt="Img my work" />
-              Title: MinhQuyCode
-            </li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/200/300" alt="Img my work" />
-              Title: MinhQuyCode
-            </li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/200/300" alt="Img my work" />
-              Title: MinhQuyCode
-            </li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/200/300" alt="Img my work" />
-              Title: MinhQuyCode
-            </li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/200/300" alt="Img my work" />
-              Title: MinhQuyCode
-            </li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-            <li>Add what you like to work ?</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ clone-trello/src/components/BoardBar/BoardBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./BoardBar.scss";
+
+const BoardBar = () => {
+  return <nav className="navbar-board">Navbar Board</nav>;
+};
+
+export default BoardBar;
