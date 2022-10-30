8d68bfbc232c22001bac61c4b5e202f988a42e1d
Quy Pham <quy.pham@hdwebsoft.co>
2022-10-30 20:07:49 +0700

    handle data

--- clone-trello/src/App.jsx
+++ clone-trello/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.scss";
 // Custom component
 import AppBar from "components/AppBar/AppBar";
--- clone-trello/src/components/Column/Column.jsx
+++ clone-trello/src/components/Column/Column.jsx
@@ -1,22 +1,47 @@
 import Card from "components/Card/Card";
 import React from "react";
 import { mapOrder } from "utils/sort";
+import { Container, Draggable } from "react-smooth-dnd";
 
 import "./Column.scss";
 
-const Column = ({ column }) => {
+const Column = ({ column, onCardDrop }) => {
   // sort columns
   const cardList = mapOrder(column.cards, column.cardOrder, "id");
 
   return (
     <div className="column">
-      <header>{column.title}</header>
-      <ul className="task-list">
-        {cardList.map((card, index) => {
-          return <Card card={card} key={index} />;
-        })}
-      </ul>
-      <footer>Add another card</footer>
+      <header className="column-drag-handle">{column.title}</header>
+      <div className="task-list">
+        <Container
+          groupName="col"
+          onDrop={(dropResult) => {
+            onCardDrop(column.id, dropResult);
+          }}
+          getChildPayload={(index) => cardList[index]}
+          dragClass="card-ghost"
+          dropClass="card-ghost-drop"
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: "drop-preview",
+          }}
+          dropPlaceholderAnimationDuration={200}
+        >
+          {cardList.map((card, index) => {
+            return (
+              <Draggable key={index}>
+                <Card card={card} />
+              </Draggable>
+            );
+          })}
+        </Container>
+      </div>
+      <footer>
+        <div className="footer-actions">
+          <i className="fa fa-plus icon" /> Add another card
+        </div>
+      </footer>
     </div>
   );
 };
